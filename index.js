09ad9389d88088a8df0c2c651efd3c413989261b
shaswatvashistha-1998 <43009138+shaswatvashistha-1998@users.noreply.github.com>
2020-11-04 12:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Sequelize = require('sequelize')
+const env=require('dotenv').config()
 const db = new Sequelize({
-  dialect:'postgres',
-  username:'postgres',
-  password:'shas8290819282',
-  host:'localhost',
-  port:5432,
-  database:'todo_database'
+  dialect:process.env.DATABASE_NAME,
+  username:process.env.DATABASE_USERNAME,
+  password:process.env.DATABASE_USER_PASSWORD,
+  host:process.env.APP_HOST,
+  port:process.env.DATABASE_PORT,
+  database:process.env.DATABASE_NAME
 });
 const blogs = db.define('blogs', {
   name: {
